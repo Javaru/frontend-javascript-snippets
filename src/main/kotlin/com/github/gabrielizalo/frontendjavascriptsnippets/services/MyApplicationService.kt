ace8d533e90a3f7c7f228dfff81413ae27e66d3a
GitHub Action <action@github.com>
2021-12-16 01:37:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/gabrielizalo/frontendjavascriptsnippets/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gabrielizalo.frontendjavascriptsnippets.services
+
+import com.github.gabrielizalo.frontendjavascriptsnippets.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
